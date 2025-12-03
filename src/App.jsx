6962f2b81e0be10aaa15613d9e227aa7a67efdc5
anjalei <anjalirnair302@gmail.com>
2025-12-03 19:52:15 +0530

    Add Fetching, Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { Routes,Route} from 'react-router'
 import './App.css'
-import { HomePage } from './pages/HomePage'
-import {CheckoutPage} from './pages/CheckoutPage'
+import { HomePage } from './pages/Home/HomePage'
+import {CheckoutPage} from './pages/Checkout/CheckoutPage'
 import {OrdersPage} from './pages/OrdersPage'
+import {TrackingPage} from './pages/TrackingPage'
+import { NotFoundPage } from './pages/NotFoundPage';
+import { useEffect,useState } from 'react'
+import axios  from 'axios';
 function App() {
+   const [cart,setCart]=useState([])
+   useEffect (()=>{
+    const fetchAppData= async()=>{
+    const response = await  axios.get('/api/cart-items?expand=product')
+     setCart(response.data)
+    }
+    fetchAppData();
+   },[])
   return (
     <>
     <Routes>
-      <Route index element={< HomePage />} />
-      <Route path='checkout' element ={<CheckoutPage />} />
-      <Route path='orders' element={<OrdersPage />}/>
-    </Routes>
+      <Route index element={< HomePage cart={ cart }/>} />
+      <Route path='checkout' element ={<CheckoutPage cart={ cart }/>} />
+      <Route path='orders' element={<OrdersPage cart={cart} />}/>
+      <Route path='tracking' element={<TrackingPage/>}  />
+       <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,29 @@
+import {Header} from '../../components/Header';
+import axios from 'axios';
+import {useEffect,useState} from 'react'
+import { ProductsGrid } from './ProductsGrid';
+ import './HomePage.css'
+ 
+ export function HomePage( { cart }){
+    const [products,setProducts]= useState([]);
+   
+    useEffect(()=>{
+        const getHomeData= async ()=>{
+        const response= await axios.get('/api/products')
+        setProducts(response.data)
+        }
+        getHomeData();
+    },[]);
+    
+    return (
+        <>
+          <title>Ecommerce Home</title>
+          <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+            <Header cart ={cart}/>
+
+        <div className="home-page">
+         <ProductsGrid products={ products }/>
+        </div>
+    </>
+    );
+}
